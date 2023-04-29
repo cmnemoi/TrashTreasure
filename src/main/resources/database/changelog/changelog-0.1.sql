a5f750b04384e2d6658aef98b3250870f1750695
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-04-29 12:35:24 +0200

    refactor: un peu de réorganisation de changelog de la BDD

--- src/main/resources/database/changelog/changelog-0.1.sql
+++ src/main/resources/database/changelog/changelog-0.1.sql
@@ -1,4 +1,7 @@
-CREATE TABLE IF NOT EXISTS users (
+-- liquibase formatted sql
+
+-- changeset liquibase:1
+CREATE TABLE IF NOT EXISTS public.user (
     id serial PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
