e7f94461bf53e2bd9143ea2a9151a6a13104bdd8
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-05-04 18:08:46 +0200

    test: ajout d'un test pour la fonction de création de copte

--- src/test/java/com/cytech/trashtreasure/service/UserServiceTests.java
+++ src/test/java/com/cytech/trashtreasure/service/UserServiceTests.java
@@ -1,18 +1,30 @@
 package com.cytech.trashtreasure.service;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.cytech.trashtreasure.repository.UserRepository;
 
 @SpringBootTest
 public class UserServiceTests {
-    
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
+
     @Test
     void createUserFromCredentialsTest() {
-        String username = "username";
-        String password = "password";
-
-        UserService userService = new UserService();
+        // generate random username and password
+        String username = String.valueOf(System.currentTimeMillis());
+        String password = String.valueOf(System.currentTimeMillis());
 
         userService.createUserFromCredentials(username, password);
+
+        assertNotNull(userRepository.findByUsername(username), "User should be found in repository");
+
     }
 }
