446faa3381f3a3d5e603a8b515127d5c6af8ccde
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-05-07 16:02:21 +0200

    refactor: retrait de code devenu inutile

--- src/main/java/com/cytech/trashtreasure/controller/HomeController.java
+++ src/main/java/com/cytech/trashtreasure/controller/HomeController.java
@@ -2,25 +2,10 @@ package com.cytech.trashtreasure.controller;
 
 import org.springframework.stereotype.Component;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import net.rgielen.fxweaver.core.FxmlView;
 
 @Component
 @FxmlView("HomeView.fxml")
 public class HomeController extends AbstractController {
     
-    @FXML
-    private Label fidelityPointsLabel;
-    @FXML
-    private Label usernameLabel;
-    
-    protected void updateScene() {
-        usernameLabel.setText(connectedUser.getUsername());
-        fidelityPointsLabel.setText(
-            "Points : " +
-            connectedUser.getFidelityPoints().toString()
-        );
-    }
-
 }
