2c78cf30ae2613c5e99771288761d5cd1a600c47
Charles-Meldhine Madi Mnemoi <63333367+cmnemoi@users.noreply.github.com>
2023-05-08 14:49:42 +0200

    feat: affichage du nom de l'utilisateur et de son solde sur toutes les fenêtres (#16)
    * feat: mise à jour de la classe `User`
    
    * feat: ajout de la base de données
    
    * feat: création d'un `AbstractController`
    
    L'`AbstractController` permet de créer d'autres contrôleurs qui récupérerent les informations de l'utilisateur et des les afficher sur toutes les fenêtres de l'application
    
    * feat: mise à jour du `LoginController` pour transmetter les informations de l'utilisateur aux autres fenêtres
    
    * feat: mise à jour du `HomeController` et de la page d'acceuil pour afficher les infos de l'utilisateur
    
    * refactor: retrait de code devenu inutile

---
+++ src/main/java/com/cytech/trashtreasure/controller/AbstractController.java
@@ -0,0 +1,48 @@
+package com.cytech.trashtreasure.controller;
+
+import org.springframework.stereotype.Component;
+
+import com.cytech.trashtreasure.entity.User;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+@Component
+abstract public class AbstractController {
+    
+    @FXML
+    private Label fidelityPointsLabel;
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    protected VBox root;
+
+    protected User connectedUser;
+
+    @FXML
+    public void initialize() {
+    }
+
+    public void setConnectedUser(User connectedUser) {
+        this.connectedUser = connectedUser;
+    }
+
+    public void show(Stage stage) {
+        stage.setScene(new Scene(root));
+        updateScene();
+        stage.show();
+    }
+
+    protected void updateScene() {
+        usernameLabel.setText(connectedUser.getUsername());
+        fidelityPointsLabel.setText(
+            "Points : " +
+            connectedUser.getFidelityPoints().toString()
+        );
+    }
+
+}
+
--- src/main/java/com/cytech/trashtreasure/controller/HomeController.java
+++ src/main/java/com/cytech/trashtreasure/controller/HomeController.java
@@ -2,26 +2,10 @@ package com.cytech.trashtreasure.controller;
 
 import org.springframework.stereotype.Component;
 
-import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxmlView;
 
 @Component
 @FxmlView("HomeView.fxml")
-public class HomeController {
+public class HomeController extends AbstractController {
     
-
-    @FXML
-    private VBox root;
-
-    @FXML
-    public void initialize() {
-    }
-
-    public void show(Stage stage) {
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
 }
