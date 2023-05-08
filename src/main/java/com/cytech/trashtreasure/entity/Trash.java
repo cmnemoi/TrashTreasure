9eea8de37e19f653fb0a880367a52b0af6c8178e
Charles-Meldhine Madi Mnemoi <63333367+cmnemoi@users.noreply.github.com>
2023-05-08 19:59:11 +0200

    feat: Ajout des classes `TrashConfig` et `Trash` (#36)
    * docs: mise à jour du diagramme de classes
    
    * feat: ajout de la classe `TrashConfig`
    
    * feat: ajout d'un script SQL pour ajouter des déchets à la BDD
    
    * feat: ajout de la classe Trash
    
    * refactor: déplacement du script d'initialisation

---
+++ src/main/java/com/cytech/trashtreasure/entity/Trash.java
@@ -0,0 +1,56 @@
+package com.cytech.trashtreasure.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Setter
+@Table(name = "trash", schema = "public")
+public class Trash {
+    
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+
+    @OneToOne(targetEntity = TrashConfig.class)
+    private TrashConfig trashConfig;
+
+    @Column(nullable = false)
+    private Date depositDate;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Integer earnedPoints;
+
+    @ManyToOne(targetEntity = User.class)
+    private User depositor;
+
+    public String getName() {
+        return trashConfig.getName();
+    }
+
+    public String getType() {
+        return trashConfig.getType();
+    }
+
+    public Float getWeight() {
+        return trashConfig.getWeight();
+    }
+}
