f173129cf916893dfe9ffbfdcb9ccf2704955570
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-05-09 19:19:17 +0200

    feat: ajout de la classe Bin

---
+++ src/main/java/com/cytech/trashtreasure/entity/Bin.java
@@ -0,0 +1,35 @@
+package com.cytech.trashtreasure.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Setter
+@Table(name = "bin", schema = "public")
+public class Bin {
+
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+
+    @Column(nullable = false)
+    private String color;
+
+    @OneToMany(targetEntity = Trash.class, mappedBy = "bin")
+    private List<Trash> trashes;
+    
+}
