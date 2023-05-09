5690a94475d3fda840938a4684ad14cbd1d853f0
Charles-Meldhine Madi Mnemoi <63333367+cmnemoi@users.noreply.github.com>
2023-05-09 16:30:49 +0200

    feat: création de la vue d'acceuil `HomeView.fxml` (#37)
    * feat: création de la vue d'acceuil `HomeView.fxml`
    
    * remove unused controllers

--- src/main/java/com/cytech/trashtreasure/controller/HomeController.java
+++ src/main/java/com/cytech/trashtreasure/controller/HomeController.java
@@ -2,10 +2,43 @@ package com.cytech.trashtreasure.controller;
 
 import org.springframework.stereotype.Component;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import net.rgielen.fxweaver.core.FxmlView;
 
 @Component
 @FxmlView("HomeView.fxml")
 public class HomeController extends AbstractController {
     
+    @FXML
+    public Button convertPointsButton;
+    @FXML
+    public Button depositButton;
+    @FXML
+    public Button watchDepositButton;
+
+    @FXML
+    public void initialize() {
+        convertPointsButton.setOnAction(actionEvent -> {
+            goToConvertPointsView();
+        });
+        depositButton.setOnAction(actionEvent -> {
+            goToDepositView();
+        });
+        watchDepositButton.setOnAction(actionEvent -> {
+            goToWatchDepositsView();
+        });
+    }
+
+    private void goToConvertPointsView() {
+        // TODO
+    }
+
+    private void goToDepositView() {
+        // TODO
+    }
+
+    private void goToWatchDepositsView() {
+        // TODO
+    }
 }
